dcbb9526e68af3283737301a97e891bac57518e4
emrantokhi <emrantokhiwork@gmail.com>
2023-08-05 17:09:28 -0700

    First commit

---
+++ pages/unreal5/capybaracrush.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function CapybaraCrush() {
+    return (
+      <>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">Back To Home!</Link>
+            </h2>
+      </>
+    );
+}
