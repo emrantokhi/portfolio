b0a0fee98cf815c7916dadd9f616412158f5531b
emrantokhi <emrantokhiwork@gmail.com>
2023-08-05 18:34:59 -0700

    Page update test

--- pages/unreal5/capybaracrush.js
+++ pages/unreal5/capybaracrush.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function CapybaraCrush() {
     return (
       <>
-        <h1>First Post</h1>
+        <h1>First Post Heyo!</h1>
         <h2>
             <Link href="/">Back To Home!</Link>
             </h2>
