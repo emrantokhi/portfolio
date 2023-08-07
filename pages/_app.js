a3824a9fc58f200aa0a775c662b45783caeb45bb
emrantokhi <emrantokhiwork@gmail.com>
2023-08-07 13:00:12 -0700

    Adjusted viewport width

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }) {
     return (
         <>
-            <meta name="viewport" content="width=450, initial-scale=1.0" />
+            <meta name="viewport" content="width=500, initial-scale=1.0" />
             <Component {...pageProps} />
             <NavBar></NavBar>
             <ScrollToTop smooth color="#6f00ff"></ScrollToTop>
