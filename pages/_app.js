25389e68e6b9a937bc4bc567848adb66ad7c61ad
emrantokhi <emrantokhiwork@gmail.com>
2023-08-07 02:30:37 -0700

    Mobile updates

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import NavBar from '/components/navbar.js'
 import ScrollToTop from "react-scroll-to-top";
 import Head from 'next/head';
 
+/*<meta name="viewport" content="width=1024" /> add this above component*/
 
 export default function App({ Component, pageProps }) {
     return (
         <>
-            <meta name="viewport" content="width=1024" />
             <Component {...pageProps} />
             <NavBar></NavBar>
             <ScrollToTop smooth color="#6f00ff"></ScrollToTop>
