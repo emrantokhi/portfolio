5e6509687cf1b2f54a3344c7165f336e9de32898
emrantokhi <emrantokhiwork@gmail.com>
2023-08-07 13:14:48 -0700

    Attempt to fix zoom in on start

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }) {
     return (
         <>
-            <meta name="viewport" content="width=500, initial-scale=1.0" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=5.0" />
             <Component {...pageProps} />
             <NavBar></NavBar>
             <ScrollToTop smooth color="#6f00ff"></ScrollToTop>
